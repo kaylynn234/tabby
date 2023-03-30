2861c542c2326fc95e51bb8e265e089112c8d3fe
Kaylynn <mkaylynn7@gmail.com>
2023-03-30 13:17:06 +1100

    Add rank card renderer and `rank` command

--- tabby/ext/levels.py
+++ tabby/ext/levels.py
@@ -1,21 +1,63 @@
+import asyncio
 import random
-from discord import Message, User
-from discord.ext.commands import BucketType, CooldownMapping, Cooldown
+from io import BytesIO
+
+from discord import File, Member, Message, User
+from discord.ext import commands
+from discord.ext.commands import BucketType, Context, CooldownMapping, Cooldown
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from yarl import URL
 
 from ..bot import Tabby, TabbyCog
 from ..level import LEVELS
+from ..util import DriverPool
+
+
+def _render_rank_card(driver: Firefox, url: URL) -> BytesIO:
+        driver.get(str(url))
+
+        buffer = BytesIO()
+        element = driver.find_element(By.CLASS_NAME, value="container")
+
+        buffer.write(element.screenshot_as_png)
+        buffer.seek(0)
+
+        return buffer
 
 
 class Levels(TabbyCog):
     cooldowns: CooldownMapping
+    drivers: DriverPool
 
     def __init__(self, bot: Tabby) -> None:
         super().__init__(bot)
 
+        self.drivers = DriverPool(driver_count=self.config.limits.webdrivers)
         self.cooldowns = CooldownMapping(
-            Cooldown(bot.config.xp_per, bot.config.xp_rate),
+            Cooldown(bot.config.level.xp_per, bot.config.level.xp_rate),
             BucketType.member,
         )
+
+    @commands.command()
+    async def rank(self, ctx: Context, who: Member | None = None):
+        """Display a member's rank and level.
+
+        who:
+            The person to display a rank for. If not provided, defaults to the message author.
+        """
+
+        if who is None:
+            assert isinstance(ctx.author, Member)
+            who = ctx.author
+
+        url = self.local_api.url_for("profiles", guild_id=who.guild.id, member_id=who.id)
+
+        async with self.drivers.get() as driver:
+            loop = asyncio.get_running_loop()
+            image = await loop.run_in_executor(None, lambda: _render_rank_card(driver, url))
+
+        await ctx.send(file=File(image, filename="rank.png"))
 
     @TabbyCog.listener()
     async def on_message(self, message: Message):
@@ -46,10 +88,6 @@ class Levels(TabbyCog):
         after = LEVELS.get(new_xp)
 
         if after.level > before.level:
-            # I have no idea if this will ever actually *happen*, but I'd rather be safe than sorry.
-            if isinstance(message.author, User):
-                victim = await message.guild.fetch_member(message.author.id)
-            else:
-                victim = message.author
+            assert isinstance(message.author, Member)
 
-            self.bot.dispatch("on_level", victim, after.level)
+            self.bot.dispatch("on_level", message.author, after.level)
