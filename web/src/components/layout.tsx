d496c7191f4cc2a4b01eb4759360ca5cc1496a9e
Kaylynn <mkaylynn7@gmail.com>
2023-04-09 12:56:28 +1000

    Update landing page

--- web/src/components/layout.tsx
+++ web/src/components/layout.tsx
@@ -37,9 +37,9 @@ export default function Layout({ children }: LayoutProps) {
           </Container>
         </Toolbar>
       </AppBar>
-      <Container>
-        <main>{children}</main>
-      </Container>
+      <main>
+        {children}
+      </main>
     </>
   )
 }
