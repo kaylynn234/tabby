7d29fecad18d74c7be419c8238c0812b148cef3c
Kaylynn <mkaylynn7@gmail.com>
2023-04-09 18:24:42 +1000

    Update landing page & begin auth impl.

--- web/src/components/layout.tsx
+++ web/src/components/layout.tsx
@@ -1,14 +1,27 @@
-import Head from 'next/head';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutIcon from '@mui/icons-material/Logout';
+import PersonIcon from '@mui/icons-material/Person';
+
 import AppBar from '@mui/material/AppBar';
 import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Person from '@mui/icons-material/Person';
-import IconButton from '@mui/material/IconButton';
+import PopupState, { bindMenu, bindTrigger } from 'material-ui-popup-state';
+import Head from 'next/head';
+import { useSession } from 'next-auth/react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Divider from '@mui/material/Divider';
+import ListItemIcon from '@mui/material/ListItemIcon';
 
 export const metadata = {
   title: 'Tabby',
@@ -20,6 +33,56 @@ type LayoutProps = {
 };
 
 export default function Layout({ children }: LayoutProps) {
+  let { status, data } = useSession();
+  let accountMenu;
+
+  if (status !== 'authenticated') {
+    accountMenu = (
+      <PopupState variant='popover'>
+        {(popupState) => (
+          <>
+            <Button
+              variant='contained'
+              startIcon={<Avatar src={data?.user?.image || undefined} />}
+              endIcon={popupState.isOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+              disableElevation
+              {...bindTrigger(popupState)}
+            >
+              {data?.user?.name || 'Account'}
+            </Button>
+            <Menu
+              {...bindMenu(popupState)}
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+              transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+            >
+              <MenuItem>
+                <ListItemIcon>
+                  <SettingsIcon fontSize='small' />
+                </ListItemIcon>
+                Dashboard
+              </MenuItem>
+              <Divider />
+              <MenuItem>
+                <ListItemIcon>
+                  <LogoutIcon fontSize='small' color='error' />
+                </ListItemIcon>
+                Log out
+              </MenuItem>
+            </Menu>
+          </>
+        )}
+      </PopupState>
+    );
+  } else {
+    accountMenu = (
+      <Paper>
+        <Button variant='outlined' startIcon={<PersonIcon />}>
+          Login with Discord
+        </Button>
+      </Paper>
+    );
+  }
+
   return (
     <>
       <AppBar position='sticky'>
@@ -30,9 +93,15 @@ export default function Layout({ children }: LayoutProps) {
               <Typography variant='h5' sx={{ flexGrow: 1, display: 'block' }}>
                 Tabby
               </Typography>
-              <Button sx={{ color: 'primary.contrastText' }}  endIcon={<Person />}>
-                Log In
-              </Button>
+              <IconButton
+                href='https://github.com/kaylynn234/tabby'
+                target='_blank'
+                rel='noopener noreferrer'
+                size='small' 
+              >
+                <GitHubIcon sx={{ color: 'primary.contrastText' }} />
+              </IconButton>
+              {accountMenu}
             </Stack>
           </Container>
         </Toolbar>
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,14 +2,35 @@ import { AppProps } from 'next/app'
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider, type PaletteColorOptions } from '@mui/material/styles';
 import Layout from '../components/layout'
+import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
+import { Session } from 'next-auth';
 
-export default function App({ Component, pageProps }: AppProps) {
+interface PageProps {
+  session?: Session
+}
+
+const theme = createTheme({
+  typography: {
+    button: {
+      textTransform: 'none'
+    }
+  }
+});
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps }
+}: AppProps<PageProps>) {
   return (
-    <CssBaseline>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </CssBaseline>
+    <SessionProvider session={session}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </CssBaseline>
+      </ThemeProvider>
+    </SessionProvider>
   )
 }
