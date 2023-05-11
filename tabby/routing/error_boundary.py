68169bf681a865880ae33983a55b780fe88183b4
Kaylynn Morgan <mkaylynn7@gmail.com>
2023-05-11 19:37:21 +1000

    Rework `ErrorBoundary` middleware

--- tabby/routing/error_boundary.py
+++ tabby/routing/error_boundary.py
@@ -1,16 +1,14 @@
-from typing import Any, Awaitable, Callable, Type, TypeVar
+import logging
+from typing import Awaitable, Callable
 
 from aiohttp import web
-from aiohttp.web import HTTPError, Request, Response, StreamResponse
-from typing_extensions import Self
-
-from pydantic import ValidationError
-
-from .exceptions import RequestValidationError
+from aiohttp.web import Request, Response, StreamResponse
 
 
-ExceptionT = TypeVar("ExceptionT", bound=Exception)
-HandlerT = Callable[[ExceptionT, Request], Awaitable[Response]]
+LOGGER = logging.getLogger(__name__)
+
+
+Handler = Callable[[Exception, Request], Awaitable[Response]]
 
 
 class ErrorBoundary:
@@ -20,10 +18,10 @@ class ErrorBoundary:
     or return different responses based on the exception that occurred.
     """
 
-    _handlers: dict[type, Callable[[Exception, Request], Awaitable[Response]]]
+    _handler: Handler
 
-    def __init__(self) -> None:
-        self._handlers = {}
+    def __init__(self, handler: Handler) -> None:
+        self._handler = handler
         # Necessary to mark the instance as a callable middleware.
         web.middleware(self)
 
@@ -40,88 +38,4 @@ class ErrorBoundary:
         try:
             return await handler(request)
         except Exception as error:
-            return await self.handle_exception(error, request)
-
-    @classmethod
-    def default(cls) -> Self:
-        """Create an `ErrorBoundary` pre-configured with error handlers for common exception types."""
-
-        new = cls()
-        new.register_handler(Exception)(_fallback_error_handler)
-        new.register_handler(HTTPError)(_handle_http_error)
-        new.register_handler(RequestValidationError)(_handle_validation_error)
-
-        return new
-
-    async def handle_exception(self, exception: Exception, request: Request) -> Response:
-        """Handle the provided exception.
-
-        This method will search through the handlers registered on this `ErrorBoundary` until it finds an exception
-        handler that matches the provided exception. If no handler matches, this method will re-raise the provided
-        exception.
-        """
-
-        exception_type = type(exception)
-
-        for base in exception_type.__mro__:
-            if base not in self._handlers:
-                continue
-
-            handler = self._handlers[base]
-
-            return await handler(exception, request)
-
-        # No handler found, so re-raise the exception
-        raise exception
-
-    def register_handler(self, exception: Type[ExceptionT]) -> Callable[[HandlerT[ExceptionT]], HandlerT[ExceptionT]]:
-        """A decorator that registers an exception handler for a specific exception type.
-
-        An exception handler is an asynchronous function that takes two parameters and returns a `Response`. The first
-        parameter is the raised exception (an `Exception` instance) and the second parameter is the `Request` that
-        caused the exception.
-
-        The decorator's `exception` argument is the exception type used for the exception handler.
-
-        If an exception is raised within an exception handler, `ErrorBoundary` will fall back to using the default
-        behaviour provided by aiohttp.
-        """
-
-        if not isinstance(exception, type):
-            raise TypeError(f"expected an exception type, but received {exception!r} instead")
-
-        if not issubclass(exception, Exception):
-            raise TypeError(f"{exception} does not derive Exception")
-
-        def inner(func: HandlerT[ExceptionT]) -> HandlerT[ExceptionT]:
-            self._handlers[exception] = func  # type: ignore
-
-            return func
-
-        return inner
-
-
-async def _fallback_error_handler(error: Exception, request: Request) -> Response:
-    return web.json_response(
-        {"error": str(error)},
-        status=500,
-    )
-
-
-async def _handle_http_error(error: HTTPError, request: Request) -> Response:
-    message = error.text or f"{error.status}: {error.reason}"
-
-    return web.json_response(
-        {"error": message},
-        status=error.status,
-        headers=error.headers,
-    )
-
-
-async def _handle_validation_error(error: RequestValidationError, request: Request) -> Response:
-    response: dict[str, Any] = {"error": error.message}
-
-    if isinstance(error.original, ValidationError):
-        response["details"] = error.original.errors()
-
-    return web.json_response(response, status=400)
+            return await self._handler(error, request)
--- tabby/web/__init__.py
+++ tabby/web/__init__.py
@@ -1,37 +1,58 @@
-from typing import Annotated
+from typing import Annotated, Any
 
 from aiohttp import web
+from aiohttp.web import HTTPError
+from pydantic import ValidationError
 
 from . import routes
 from .session import SessionStorage
 from .. import routing
 from ..bot import Tabby
-from ..resources import STATIC_DIRECTORY
-from ..routing import Application, ErrorBoundary, Use
+from ..routing import Application, ErrorBoundary, Request, Response, Use
+from ..routing.exceptions import RequestValidationError
 
 
 def setup_application(bot: Tabby) -> Application:
     """Build and configure an `Application` instance for the provided `bot`."""
 
     middlewares = [
-        ErrorBoundary.default(),
+        ErrorBoundary(error_handler),
         SessionStorage(bot),
     ]
 
     app = Application(middlewares=middlewares)
     app["bot"] = bot
 
-    app.add_routes([
-        routes.auth_callback,
-        routes.user_guilds,
-        routes.member_profile,
-        routes.guild_leaderboard,
-        web.static("/", STATIC_DIRECTORY),
-    ])
 
-    bot._api = app
 
-    return app
+async def error_handler(error: Exception, request: Request) -> Response:
+    if isinstance(error, RequestValidationError):
+        payload: dict[str, Any] = {"error": error.message}
+
+        if isinstance(error.original, ValidationError):
+            payload["details"] = error.original.errors()
+
+        response = web.json_response(payload, status=400)
+    elif isinstance(error, HTTPError):
+        message = error.text or f"{error.status}: {error.reason}"
+        headers = error.headers.copy()
+
+        if "Content-Type" in headers:
+            del headers["Content-Type"]
+
+        response = web.json_response({"error": message}, status=error.status, headers=headers)
+    else:
+        response = web.json_response({"error": str(error)}, status=500)
+
+    no_error_page = ("/api/", "/oauth/")
+
+    if any(request.path.startswith(prefix) for prefix in no_error_page):
+        return response
+
+    return Response(
+        text=f"{response.status} {response.reason}",
+        status=response.status,
+    )
 
 
 @routing.register_extractor(Tabby)
